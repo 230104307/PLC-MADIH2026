32da17ff8a703ee41f9e36018612ef42d53b952d
230104307 <230104307@aston.ac.uk>
2026-02-06 12:49:26 +0000

    task1b

--- lab4-primit/javascript/ErrorsEnum.js
+++ lab4-primit/javascript/ErrorsEnum.js
@@ -16,17 +16,17 @@ const Error_enumobj = {
 
 function error2Result(err){
     switch (err) {
-	case Result_enumobj.FP_ROUNDING:
-	 return Error_enumobj.A_BIT_DIFFERENT;
+	case Result_enumobj.A_BIT_DIFFERENT:
+	 return Error_enumobj.FP_ROUNDING;
 	break;
-	case Result_enumobj.FP_OVERFLOW:
-	    return Error_enumobj.INFINITY;
+	case Result_enumobj.INFINITY:
+	    return Error_enumobj.FP_OVERFLOW;
 	break;
-	case Result_enumobj.FP_UNDERFLOW:
-	    return Error_enumobj.ZERO;
+	case Result_enumobj.ZERO:
+	    return Error_enumobj.FP_UNDERFLOW;
 	break;
-	case Result_enumobj.INT_OVERFLOW:
-	    return Error_enumobj.VERY_DIFFERENT;
+	case Result_enumobj.VERY_DIFFERENT:
+	    return Error_enumobj.INT_OVERFLOW;
 	break;
 	default:
 		return 'Invalid Error value';
