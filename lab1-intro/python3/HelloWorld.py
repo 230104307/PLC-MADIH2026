a4bcb2c4c08e6cc9c5f61951ffecd6d17da0dbbb
230104307 <230104307@aston.ac.uk>
2026-02-07 16:49:17 +0000

    Lab1 revision

--- lab1-intro/python3/HelloWorld.py
+++ lab1-intro/python3/HelloWorld.py
@@ -3,4 +3,6 @@ print("Hello World! This is Python3")
 the_world_is_flat = False
 if the_world_is_flat:
     print("Be careful not to fall off!")
+else:
+    print("World is not flat, don't be silly")
 
