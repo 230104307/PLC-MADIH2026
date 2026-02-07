f52c1ae37fb1cd0d9fce21ad1ad78acf4c516ed8
230104307 <230104307@aston.ac.uk>
2026-02-07 16:49:50 +0000

    Lab1 revision

--- lab1-intro/python3/HelloWorld.py
+++ lab1-intro/python3/HelloWorld.py
@@ -1,6 +1,6 @@
 # -*- coding: cp1252 -*-
 print("Hello World! This is Python3")
-the_world_is_flat = False
+the_world_is_flat = True
 if the_world_is_flat:
     print("Be careful not to fall off!")
 else:
