80b74329ef6a464c6d16f8494074902bbbf30720
230104307 <230104307@aston.ac.uk>
2026-02-08 16:44:10 +0000

    lab02 revision

--- lab2-functionalPrimer/python3/func_as_arg.py
+++ lab2-functionalPrimer/python3/func_as_arg.py
@@ -1,11 +1,11 @@
-def listFunc() :
+def listFunc(a, b) :
 #creating a list of ints from a to b equivelent to task5 Haskell
-    return [i for i in range(a, b)]
+    return [i for i in range(a, b+1)]
 
-def applicatorFunc(inpFunc, s):
+def applicatorFunc(inpFunc, s, a , b):
     if s== 's' :
         return sum(inpFunc(a, b))
     else:
-        return sum (inpFunc(a, b))/5
+        return sum (inpFunc(a, b))/(b-a+1)
     
-print (applicatiorFunc(listFunc, 's'))
+print (applicatiorFunc(listFunc, 'a' , 1, 10))
