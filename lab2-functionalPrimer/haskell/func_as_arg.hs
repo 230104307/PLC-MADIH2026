70bfdbc40ede28aae0b26711f6d2b914efacdd8b
230104307 <230104307@aston.ac.uk>
2026-02-08 14:58:13 +0000

    lab02 revision

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -6,5 +6,5 @@ inpFunc a b = [a..b]
 applicatorFunc inpFunc a b s | s=='s'  = sum (inpFunc a b) 
                              | otherwise = (sum (inpFunc a b))/(b-a+1)              
 main = do
-  let result = applicatorFunc inpFunc 1 5 's' --Call applicatorFunc with inpFunc, integers and char as args
+  let result = applicatorFunc inpFunc 1 10 's' --Call applicatorFunc with inpFunc, integers and char as args
   putStrLn("sum = " ++ show result)
